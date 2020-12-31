65035088b9f94210e7f99917e9b23325667ad37b
MaxTheTomas <48125186+MaxTheTomas@users.noreply.github.com>
2020-12-31 20:29:00 +0300

    switch to countdown color changer

--- tests/timer/scripts/styles.js
+++ tests/timer/scripts/styles.js
@@ -3,18 +3,22 @@ var countDownTime = new Date("Jan 1, 2021 00:00:00").getTime();
 var r_ = 0;
 var g_ = 0;
 var b_ = 0;
+var doSwitch = false;
 
 var b = setInterval(function() {
+        if (!doSwitch) return
+
         if (r_ < 255 && g_ === 0 && b_ === 0) r_++;
         if (r_ >= 255 && g_ < 255 && b_ === 0) g_++;
         if (r_ > 0 && g_ >= 255 && b_ === 0) r_--;
         if (r_ === 0 && g_ >= 255 && b_ < 255) b_++;
         if (r_ === 0 && g_ > 0 && b_ >= 255) g_--;
         if (r_ === 0 && g_ === 0 && b_ > 0) b_--;
-
         document.body.style.background = rgbToHex(r_,g_,b_)
-    console.log(rgbToHex(r_,g_,b_))
-}, 7)
+        console.log(rgbToHex(r_,g_,b_))
+}, 100)
+
+function onSwitch() {doSwitch = !doSwitch}
 
 function rgbToHex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);

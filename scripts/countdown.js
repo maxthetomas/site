a785210ae992973db2a24118b6c68a1bf478e358
MaxTheTomas <48125186+MaxTheTomas@users.noreply.github.com>
2021-02-17 22:09:06 +0300

    jpp7 section

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -3,7 +3,7 @@ const maybePluralize = (count, noun, suffix = 's') =>
 
 // Set the date we're counting down to
 // var countDownDate = new Date("Mar 26, 2021 16:00:00").getTime();
-var countDownDate = new Date("Jan 1, 2021 00:00:00").getTime();
+var countDownDate = new Date("Feb 20, 2021 20:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -26,6 +26,6 @@ var x = setInterval(function() {
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdowntext").innerHTML = "С 2К21!";
+        document.getElementById("countdowntext").innerHTML = "Если этот текст до сих пор не стал ссылкой, значит Макс что-то делает)";
     }
 }, 1000);
